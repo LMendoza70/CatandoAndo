273be3e9d17b635c35519e308c29df1ce00a7774
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-03-15 13:28:17 -0600

    Changes to be committed: 	modified:   Pantallas/Home.js
     Changes not staged for commit:
    	modified:   Pantallas/Detalle.js
    	modified:   Pantallas/Estilos.js
    	modified:   Pantallas/Splash.js

--- Pantallas/Home.js
+++ Pantallas/Home.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image,TouchableOpacity } from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
 import React from 'react'
 import { Colors, Estilos } from './Estilos'
@@ -9,28 +9,31 @@ const Home = () => {
       <View style={Estilos.container}>
         <Image source={require('../assets/logo-catando-ando-coffee.png')} style={Estilos.logo}/>
         <Text style={Estilos.Titulo}>CAFE DE ESPECIALIDAD MEXICANO</Text>
-        <view style={Estilos.card}>
-          <Image source={require('../assets/luis.png')}/>
+        <View style={Estilos.card}>
+          <Image source={require('../assets/luis.png')} style={Estilos.foto}/>
           <Text style={Estilos.texto}>Seleccionamos y ofrecemos, para nuestros visitantes, 
           finos granos mexicanos de café de especialidad, cultivado en Veracruz y tostado artesanalmente 
           por el galardonado experto Luis Murillo.</Text>
-        </view>
+        </View>
         <ScrollView horizontal>
-        <TouchableOpacity style={Estilos.card}>
-          <Image source={require('../assets/cafe1.png')}/>
-          <Text>Café Garnica - Espinal Veracruz</Text>
-          <Text>Desde $180.00 MXN </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={Estilos.card}>
-          <Image source={require('../assets/cafe2.png')}/>
-          <Text>Café Garnica - Espinal Veracruz</Text>
-          <Text>Desde $180.00 MXN </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={Estilos.card}>
-          <Image source={require('../assets/cafe-3.png')}/>
-          <Text>Café Garnica - Espinal Veracruz</Text>
-          <Text>Desde $180.00 MXN </Text>
-        </TouchableOpacity>
+          
+          <TouchableOpacity style={Estilos.card} >
+            <Image source={require('../assets/cafe1.png')} style={Estilos.foto}/>
+            <Text>Café Garnica - Espinal Veracruz</Text>
+            <Text>Desde $180.00 MXN </Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={Estilos.card}>
+            <Image source={require('../assets/cafe2.png')} style={Estilos.foto}/>
+            <Text>Café Garnica - Espinal Veracruz</Text>
+            <Text>Desde $180.00 MXN </Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style={Estilos.card}>
+            <Image source={require('../assets/cafe-3.png')} style={Estilos.foto}/>
+            <Text>Café Garnica - Espinal Veracruz</Text>
+            <Text>Desde $180.00 MXN </Text>  
+          </TouchableOpacity>
         </ScrollView>
         <View style={{flexDirection:'row',justifyContent:'space-around',alignSelf:'stretch',padding:10}}>
           <Ionicons name='logo-facebook' size={25} color={Colors.primary}/>
@@ -38,7 +41,6 @@ const Home = () => {
           <Ionicons name='logo-instagram' size={25} color={Colors.primary}/>
           <Ionicons name='logo-youtube' size={25} color={Colors.primary}/>
       </View>
-        <Text>Home</Text>
       </View>
     </ScrollView>
   )
